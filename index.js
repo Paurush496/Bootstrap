af7ca556ef873347ff40b6e91cec8d2a09ffa837
Paurush Choudhary <paurushchoudhary496@gmail.com>
2018-09-06 14:59:05 +0530

    KUCH BHI

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import './node_modules/popper.js/dist/umd/popper';
 import 'bootstrap'
 import './node_modules/bootstrap/scss/bootstrap.scss'
 import './scss/index.scss';
-$('#example').popover(options);
+
