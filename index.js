29179cc103ba2c620463852776183c535e9bc108
Paurush Choudhary <paurushchoudhary496@gmail.com>
2018-09-06 12:20:14 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// import 'bootstrap';
+import './node_modules/jquery/dist/jquery';
+import './node_modules/popper.js/dist/umd/popper';
+import 'bootstrap'
+import './node_modules/bootstrap/scss/bootstrap.scss'
+import './scss/index.scss';
+$('#example').popover(options);
